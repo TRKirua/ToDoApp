b27a49c6ec7d4e7a8eed08b6dca0b6899e841868
TRKirua <enes.ekici@epita.fr>
2025-07-17 15:07:21 +0200

    v6

--- src/js/supabase.js
+++ src/js/supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL     = process.env.SUPABASE_URL
+const SUPABASE_URL = process.env.SUPABASE_URL
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY
 
 export const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
